061dd6593a63e2832d997c0c3d82cae7c2fb834a
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-10-05 06:25:24 -0300

    first commit

---
+++ components/date.tsx
@@ -0,0 +1,15 @@
+import { parseISO, format } from 'date-fns';
+
+type Props = {
+    dateString: string
+}
+
+export default function Data({ dateString }: Props) {
+    const date = parseISO(dateString);
+
+    return (
+        <time dateTime={dateString} >
+            {format(date, 'LLLL d, yyyy')}
+        </time>
+    )
+}
